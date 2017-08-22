a1391adb2df9563af40128ba3ffa346092e0ac4b
Adrian Marinica <apmarinica@gmail.com>
2017-08-22 13:42:45 +0300

    Added a (failing) unit test for the controller

---
+++ app/test/controller.spec.ts
@@ -0,0 +1,32 @@
+﻿import "angular-mocks";
+import { IModule, mock, module } from "angular";
+
+describe('weather controller', () => {
+
+    var $rootScope: any;
+    var $controller: any;
+
+    var testDateValue = '2017/8/22';
+
+    let app = module("app", []);
+    require('../src/controller')(app);
+
+    beforeEach(mock.module('app'));
+
+    beforeEach(mock.inject((_$controller_: any, _$rootScope_: any) => {
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should initialize correctly', () => {
+        $controller('weatherController', {
+            $scope: $rootScope,
+            date: {
+                getDateString: function () {
+                    return testDateValue;
+                }
+            }
+        });
+        expect($rootScope.getDate()).toBe(testDateValue);
+    });
+});
